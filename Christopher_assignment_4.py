aedcae8daa252c0f57d9d0f776248818fa8351b2
Chrisrich007 <167947766+Chrisrich007@users.noreply.github.com>
2025-09-24 22:04:58 -0400

    Delete Christopher_assignment_4.py

--- Christopher_assignment_4.py
+++
@@ -1,6 +0,0 @@
-# === REQUIRED VARIABLES ===
-student_name = "Christopher Arnold"       
-current_gpa = 3.48                  
-study_hours = 25                     
-social_points = 49                  
-stress_level = "20"           
