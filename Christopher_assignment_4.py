306261ed7839d604f5319900002dbe2df2a77e56
Chrisrich007 <167947766+Chrisrich007@users.noreply.github.com>
2025-09-24 22:00:27 -0400

    Chrisrich007_assignment_4.py
    git commit -m "Pass Test Case 1: Initial game setup with required variables"

---
+++ Christopher_assignment_4.py
@@ -0,0 +1,6 @@
+# === REQUIRED VARIABLES ===
+student_name = "Christopher Arnold"       
+current_gpa = 3.48                  
+study_hours = 25                     
+social_points = 49                  
+stress_level = "20"           
