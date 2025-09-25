123907b6bc8eac7dbc25cc9fa3dc9b8a16003b52
Chrisrich007 <167947766+Chrisrich007@users.noreply.github.com>
2025-09-25 14:43:52 -0400

    git add carnold_assignment_3.py
    git commit -m "Pass Test Case 1: Initial game setup with required variables"

---
+++ carnold_assignment_3.py
@@ -0,0 +1,7 @@
+student_name = "Christopher Arnold"       
+current_gpa = 3.48                  
+study_hours = 25                     
+social_points = 49                  
+stress_level = "20"           
+print(f"Welcome, {student_name}! Starting stats:")
+print(f"GPA: {current_gpa}, Study Hours: {study_hours}, Social Points: {social_points}, Stress Level: {stress_level}")
